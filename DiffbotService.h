a4e0527c19f07800e7e45bfb15f0cdc355aa8457
Vladimir Obrizan <obrizan@dnt-lab.com>
2014-01-02 15:58:21 +0200

    Initial commit.

---
+++ DiffbotService.h
@@ -0,0 +1,13 @@
+//
+//  DiffbotService.h
+//  DiffBotDemo
+//
+//  Created by Vladimir Obrizan on 02.01.14.
+//  Copyright (c) 2014 Design and Test Lab. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DiffbotService : NSObject
+
+@end
---
+++ DiffbotService.m
@@ -0,0 +1,13 @@
+//
+//  DiffbotService.m
+//  DiffBotDemo
+//
+//  Created by Vladimir Obrizan on 02.01.14.
+//  Copyright (c) 2014 Design and Test Lab. All rights reserved.
+//
+
+#import "DiffbotService.h"
+
+@implementation DiffbotService
+
+@end
